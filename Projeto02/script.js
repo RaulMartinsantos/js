995cd4f55e78baeb463d2d60bb072125811b8a71
Raul <raullmdsantos@gmail.com>
2026-05-04 16:54:58 -0300

    criando um objeto com os valores do form

--- Projeto02/script.js
+++ Projeto02/script.js
@@ -1,5 +1,8 @@
 //seleciona os elementos do formulário
+const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
+const expense = document.querySelector("#expense");
+const category = document.querySelector("#category");
 
 //Permite apenas inputs numéricos e já formatado para real
 amount.addEventListener("input", () => {
@@ -10,10 +13,36 @@ amount.addEventListener("input", () => {
   amount.value = formatCurrencyBRL(value);
 });
 
+//formata moeda para o padrão real
 function formatCurrencyBRL(value) {
   value = value.toLocaleString("pt-BR", {
     style: "currency",
     currency: "BRL",
   });
   return value;
+}
+
+//Capturando e salvando o evento de input em um objeto
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  expenseAdd(newExpense);
+});
+
+//Adiciona os itens na lista
+function expenseAdd(newExpense) {
+  try {
+  } catch (err) {
+    alert("Erro tente novamente mais tarde");
+    console.log(err);
+  }
 }
