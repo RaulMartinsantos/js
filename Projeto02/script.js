2f6ab0c2cc42a7ad410001b00e06a1dc9b77d80c
Raul <raullmdsantos@gmail.com>
2026-05-04 16:31:20 -0300

    Verificando e sintetizando o campo de valor da despesa

--- Projeto02/script.js
+++ Projeto02/script.js
@@ -1,8 +1,19 @@
 //seleciona os elementos do formulário
-
 const amount = document.querySelector("#amount");
 
+//Permite apenas inputs numéricos e já formatado para real
 amount.addEventListener("input", () => {
   let value = amount.value.replace(/\D/g, "");
-  amount.value = value;
+
+  value = Number(value) / 100;
+
+  amount.value = formatCurrencyBRL(value);
 });
+
+function formatCurrencyBRL(value) {
+  value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  return value;
+}
