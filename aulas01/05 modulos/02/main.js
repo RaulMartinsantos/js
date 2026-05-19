c2f27f592dce4a612873d5f69f3ddf14992279b3
Raul <raullmdsantos@gmail.com>
2026-05-19 19:55:53 -0300

    Aulas

--- aulas01/05 modulos/02/main.js
+++ aulas01/05 modulos/02/main.js
@@ -1,24 +1,5 @@
-import { calc } from "./calc.js";
+const now = dayjs().format("DD/MM/YYYY - HH:MM");
 
-const newCalc = new calc();
+console.log(now);
 
-console.log(`4 + 6 = ${newCalc.sum(4, 6)}`);
 
-console.log(`6 x 9 = ${newCalc.multiply(6, 9)}`);
-console.log(newCalc.name);
-
-setTimeout(() => {
-  console.log("Variaz vez");
-}, 2000);
-
-let value = 10;
-
-const interval = setInterval(() => {
-  console.log(value);
-  value--;
-
-  if (value === 0) {
-    console.log("tempo expirado");
-    clearInterval(interval);
-  }
-}, 1000);
