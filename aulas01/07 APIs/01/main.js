bc8fa510ca7421c71f1176d0f74b1105cfa81e39
Raul <raullmdsantos@gmail.com>
2026-05-21 17:06:43 -0300

    Aulas

---
+++ aulas01/07 APIs/01/main.js
@@ -0,0 +1,36 @@
+fetch("http://localhost:3333/products/")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+async function products() {
+  const response = await fetch("http://localhost:3333/products/");
+  const data = await response.json();
+
+  console.log(data);
+}
+
+async function fetchProductById(id) {
+  const response = await fetch(`http://localhost:3333/products/${id}`);
+  const data = await response.json();
+  console.log(data);
+}
+
+const productName = document.querySelector("#name");
+const productPrice = document.querySelector("#price");
+const form = document.getElementsByTagName("form")[0];
+
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+
+  await fetch("http://localhost:3333/products/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: new Date().getTime().toString(),
+      name: productName.value,
+      price: Number(productPrice.value),
+    }),
+  });
+});
