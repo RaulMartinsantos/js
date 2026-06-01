35d0c759455344e8fca07a0f75391370b9559021
Raul <raullmdsantos@gmail.com>
2026-06-01 18:49:23 -0300

    Updates

--- Projeto03/src/modules/form/hours-load.js
+++ Projeto03/src/modules/form/hours-load.js
@@ -1,6 +1,8 @@
 import { openingHours } from "../../utils/opening-hours.js";
 import dayjs from "dayjs";
 
+const hours = document.getElementById("hours");
+
 export function hoursLoad({ date }) {
   const opening = openingHours.map((hour) => {
     const [scheduleHour] = hour.split(":");
@@ -9,9 +11,33 @@ export function hoursLoad({ date }) {
 
     return {
       hour,
-      avaliable: isHourPast,
+      available: isHourPast,
     };
   });
 
-  console.log(opening);
+  opening.forEach(({ hour, available }) => {
+    const li = document.createElement("li");
+    li.classList.add("hour");
+    li.classList.add(available ? "hour-available" : "hour-unavailable");
+
+    li.textContent = hour;
+
+    if (hour === "9:00") {
+      hourHeaderAdd("Manhã");
+    }else if (hour === "13:00"){
+      hourHeaderAdd("Tarde")
+    }else if (hour === "18:00") {
+      hourHeaderAdd("Noite")
+    }
+    hours.append(li);
+  });
+}
+
+function hourHeaderAdd(title) {
+  const header = document.createElement("li");
+
+  header.classList.add("hour-period");
+  header.textContent = title;
+
+  hours.append(header);
 }
