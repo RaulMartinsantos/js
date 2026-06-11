a68d603eae1be188a8ad72eb2c5cf0ff4e0890a6
Raul <raullmdsantos@gmail.com>
2026-06-11 14:15:02 -0300

    Aulas

---
+++ Projeto04/src/server.js
@@ -0,0 +1,10 @@
+import http from "node:http";
+import { jsonHandler } from "./middlewares/jsonHandler.js";
+import { routeHandler } from "./middlewares/routeHandler.js";
+
+const server = http.createServer(async (request, response) => {
+  await jsonHandler(request, response);
+  routeHandler(request, response);
+});
+
+server.listen(3333);
