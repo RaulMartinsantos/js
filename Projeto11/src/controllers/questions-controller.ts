abe7f4e703a142edae4d0827f48f59be58a0ec9b
Raul <raullmdsantos@gmail.com>
2026-07-20 15:41:57 -0300

    Add question controller

--- Projeto11/src/controllers/questions-controller.ts
+++ Projeto11/src/controllers/questions-controller.ts
@@ -1,21 +1,29 @@
-import { Request, Response } from "express"
+import { Request, Response } from "express";
+import { prisma } from "@/prisma";
 
 class QuestionsController {
   async index(request: Request, response: Response) {
-    return response.json()
+    const questions = await prisma.questions.findMany();
+    return response.json(questions);
   }
 
   async create(request: Request, response: Response) {
-    return response.status(201).json()
+    const { title, content, user_id } = request.body;
+    await prisma.questions.create({
+      data: { title, content, userID: user_id },
+    });
+    return response.status(201).json({
+      message: `Question title: ${title}, Question: ${content}, from user_id: ${user_id}`,
+    });
   }
 
   async update(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 
   async remove(request: Request, response: Response) {
-    return response.json()
+    return response.json();
   }
 }
 
-export { QuestionsController }
+export { QuestionsController };
