5ff76320fe5d294e147674b95e294c6050456fba
Raul <raullmdsantos@gmail.com>
2026-07-24 19:27:19 -0300

    Add authorizantion role based

--- Projeto14/src/routes/deliveries-routes.ts
+++ Projeto14/src/routes/deliveries-routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { ensureAuth } from "@/middlewares/ensure-Authenticated";
 import { DeliversController } from "@/controllers/deliveries-controller";
+import { verifyUserAuthorization } from "@/middlewares/verifyUserAuthorization";
 
 const deliveriesRoutes = Router();
 const deliveriesControllers = new DeliversController();
 
-deliveriesRoutes.use(ensureAuth);
+deliveriesRoutes.use(ensureAuth, verifyUserAuthorization(["sale"]));
 deliveriesRoutes.post("/", deliveriesControllers.create);
 
 export { deliveriesRoutes };
