8d1b24e85e660c50ccdf6774f13eb7e574249da0
Raul <raullmdsantos@gmail.com>
2026-08-12 15:50:33 -0300

    Add show route for uploads

--- Projeto16/src/app.ts
+++ Projeto16/src/app.ts
@@ -3,11 +3,14 @@ import "express-async-errors";
 import express from "express";
 import { routes } from "@/routes/index";
 import { errorHandling } from "@/middlewares/error-handling";
+import uploadConfig from "@/config/upload";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/uploads", express.static(uploadConfig.UPLOADS_FOLDER));
 
 app.use(routes);
 app.use(errorHandling);
